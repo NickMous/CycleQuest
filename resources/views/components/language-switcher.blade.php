82f8075bfc0bfb8965f1579b6015e1684a2fc706
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-03-13 21:09:45 +0100

    Laravel 11 upgrade

---
+++ resources/views/components/language-switcher.blade.php
@@ -0,0 +1,31 @@
+<div class="ms-3 relative">
+    <x-dropdown align="right" width="60">
+        <x-slot name="trigger">
+                                <span class="inline-flex rounded-md">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-text dark:text-dm-text bg-pr/20 dark:bg-dm-pr/20 hover:text-text-700 dark:hover:text-dm-text-300 focus:outline-none focus:bg-pr/60 dark:focus:bg-pr/60 active:bg-pr/50 dark:active:bg-dm-pr/50 transition ease-in-out duration-150">
+                                        {{ App::getLocale() }}
+
+                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"/>
+                                        </svg>
+                                    </button>
+                                </span>
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="w-60">
+                <div class="block px-4 py-2 text-xs text-text-600 dark:text-dm-text-300">
+                    {{ __('Available languages') }}
+                </div>
+
+                <x-dropdown-button wire:click="changeLocale('en')">
+                    {{ __('English') }}
+                </x-dropdown-button>
+
+                <x-dropdown-button wire:click="changeLocale('nl')">
+                    {{ __('Dutch') }}
+                </x-dropdown-button>
+            </div>
+        </x-slot>
+    </x-dropdown>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', './vendor/laravel/jetstream/**/*.blade.php', './storage/framework/views/*.php', './resources/views/**/*.blade.php',],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
     theme: {
         extend: {
