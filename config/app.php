3a679aab15c47cbca23c672ac519ad1eda8cfd41
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-04-10 21:30:37 +0200

    Update

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'GMT+1'),
+    'timezone' => env('APP_TIMEZONE', 'Europe/Amsterdam'),
 
     /*
     |--------------------------------------------------------------------------
