9a4e8a41e23b42e304fef151c8ee81db21c2f6d3
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-05-24 11:18:55 +0200

    Add sitemap generator

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Sitemap\SitemapGenerator;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('sitemap:generate', function () {
+    SitemapGenerator::create(config('app.url'))
+        ->writeToFile(public_path('sitemap.xml'));
+})->purpose('Generate the sitemap.')->daily();
