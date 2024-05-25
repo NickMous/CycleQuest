de8200ec08df61e98c392ea03b65ebc23fc3acf4
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-05-25 14:12:31 +0200

    Created Progress

---
+++ database/migrations/2024_05_25_120858_create_progress_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('route_id')->constrained()->cascadeOnDelete();
+            $table->integer('progress')->default(0);
+            $table->foreignId('location_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('question_id')->nullable()->constrained()->nullOnDelete();
+            $table->integer('score')->default(0);
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('progress');
+    }
+};
