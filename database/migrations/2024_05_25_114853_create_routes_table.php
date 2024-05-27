2dff9eaa595c04a9d4bb4f0511ed831ead13d5f7
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-05-27 18:29:02 +0200

    Change some strings to text

--- database/migrations/2024_05_25_114853_create_routes_table.php
+++ database/migrations/2024_05_25_114853_create_routes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('routes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('length')->nullable();
             $table->integer('duration')->nullable();
--- database/migrations/2024_05_25_115853_create_questions_table.php
+++ database/migrations/2024_05_25_115853_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('checkpoint_id')->constrained()->cascadeOnDelete();
-            $table->string('question');
+            $table->text('question');
             $table->enum('type', ['text', 'radio', 'checkbox']);
             $table->integer('order')->default(0);
             $table->timestamps();
