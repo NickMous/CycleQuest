824dbf8e63b3cc6f8eb08de9e55bb3251b4e98ed
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-05-27 10:47:43 +0200

    Renamed locations to checkpoints

--- database/factories/CheckpointFactory.php
+++ database/factories/CheckpointFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Location>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Checkpoint>
  */
-class LocationFactory extends Factory
+class CheckpointFactory extends Factory
 {
     /**
      * Define the model's default state.
