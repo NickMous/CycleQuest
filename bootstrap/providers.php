42d523af4e411c318933f7d0d2b9ac5169a5ad4a
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-06-02 16:04:52 +0200

    Install telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,6 @@ return [
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HealthServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
-    App\Providers\HealthServiceProvider::class,
 ];
