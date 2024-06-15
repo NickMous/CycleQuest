3a16fead76933a6eff0902a1246311d57d5938f5
Sander den Hollander :) <sanderdenhollander12@gmail.com>
2024-06-15 17:15:08 +0200

    Created under construction page

--- app/Livewire/Misc/UnderConstruction.php
+++ app/Livewire/Misc/UnderConstruction.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire\Misc;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class UnderConstruction extends Component
 {
+    #[Title('Under Construction')]
     public function render()
     {
-        return view('livewire.misc.under-construction');
+        return view('livewire.misc.under-construction')->layout('layouts.app');
     }
 }
